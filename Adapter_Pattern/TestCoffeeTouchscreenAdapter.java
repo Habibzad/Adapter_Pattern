576099f35089f60e0ef4eff942d58da9491f5d79
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-04-01 23:05:46 -0400

    Update

--- Adapter_Pattern/TestCoffeeTouchscreenAdapter.java
+++ Adapter_Pattern/TestCoffeeTouchscreenAdapter.java
@@ -2,9 +2,9 @@ public class TestCoffeeTouchscreenAdapter {
 
     public static void main(String[] args) {
         OldCoffeeMachine machine = new OldCoffeeMachine();
-        CoffeeTouchscreenAdapter ctsa = new CoffeeTouchscreenAdapter(machine);
-        ctsa.chooseFirstSelection(); // Prints "Making coffee A"
-        ctsa.chooseSecondSelection(); // Prints "Making coffee B"
+        CoffeeTouchscreenAdapter coffeeTouchscreenAdapter = new CoffeeTouchscreenAdapter(machine);
+        coffeeTouchscreenAdapter.chooseFirstSelection(); // Prints "Making coffee A"
+        coffeeTouchscreenAdapter.chooseSecondSelection(); // Prints "Making coffee B"
     }
 
 }
